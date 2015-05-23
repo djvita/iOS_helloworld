25394e589c7d529e2a7b4bc84cbdbb10b35137fa
Vita V <dj.vita.09@gmail.com>
2015-05-23 11:05:14 -0700

    Initial Commit

---
+++ iOS_helloworld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS_helloworld
+//
+//  Created by vita on 5/23/15.
+//  Copyright (c) 2015 vita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS_helloworld/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOS_helloworld
+//
+//  Created by vita on 5/23/15.
+//  Copyright (c) 2015 vita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
